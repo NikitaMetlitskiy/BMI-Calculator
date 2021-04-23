a42ee7deeb32501158386e42d4edf7be252490a1
Nikita Metlitskiy <nikita.metlitskiy@gmail.com>
2021-04-23 10:47:00 +0300

    add pet project

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Mac on 22.04.2021.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
